45bd3697e3c27143357e68e8e9653518d9a0851e
mohammedchakir <zerroukimedchakir@gmail.com>
2023-07-18 06:07:18 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,15 +5,16 @@
  */
 void print_alphabet_x10(void)
 {
-	char letter;
-	int i;
+    char letter;
+    int i;
 
-	for (i = 0; i < 10; i++)
-	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-		{
-			_putchar(letter);
-		}
-	}
-	_putchar('\n');
+    for (i = 0; i < 10; i++)
+    {
+        for (letter = 'a'; letter <= 'z'; letter++)
+        {
+            _putchar(letter);
+            if (letter == 'j') // Print a new line after every 10 characters (j is the 10th character in the alphabet)
+                _putchar('\n');
+        }
+    }
 }
