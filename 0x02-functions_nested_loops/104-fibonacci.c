56ae995a854d275284cbf28a3080fb5f0456a95f
mohammedchakir <zerroukimedchakir@gmail.com>
2023-07-18 13:45:27 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main - Entry point of the program
@@ -9,18 +10,46 @@
 int main(void)
 {
 	int count;
-	unsigned long int prev1 = 1, prev2 = 2, current;
+	char fib[98][1000];
 
-	printf("%lu, %lu", prev1, prev2);
+	strcpy(fib[0], "1");
+	strcpy(fib[1], "2");
 
 	for (count = 2; count < 98; count++)
 	{
-		current = prev1 + prev2;
+		char temp[1000];
+		int carry = 0, len1, len2, sum, i, j;
 
-		printf(", %lu", current);
+		len1 = strlen(fib[count - 1]);
+		len2 = strlen(fib[count - 2]);
 
-		prev1 = prev2;
-		prev2 = current;
+		for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0 || carry; i--, j--)
+		{
+			sum = carry;
+
+			if (i >= 0)
+				sum += fib[count - 1][i] - '0';
+
+			if (j >= 0)
+				sum += fib[count - 2][j] - '0';
+
+			carry = sum / 10;
+			temp[i + 1] = (sum % 10) + '0';
+		}
+
+		if (carry)
+			temp[0] = carry + '0';
+		else
+			temp[0] = '0';
+
+		strcpy(fib[count], &temp[carry ? 0 : 1]);
+	}
+
+	printf("%s", fib[0]);
+
+	for (count = 1; count < 98; count++)
+	{
+		printf(", %s", fib[count]);
 	}
 
 	printf("\n");
