faf623414ca1ce3de0dc1dd78247fd54a24010a3
mohammedchakir <zerroukimedchakir@gmail.com>
2023-07-18 12:22:43 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,17 +7,13 @@
  */
 int main(void)
 {
-    int sum = 0;
+	int i, sum;
 
-    for (int i = 0; i < 1024; i++)
-    {
-        if (i % 3 == 0 || i % 5 == 0)
-        {
-            sum += i;
-        }
-    }
-
-    printf("%d\n", sum);
-
-    return (0);
+	for (i = 1; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+	return (0);
 }
