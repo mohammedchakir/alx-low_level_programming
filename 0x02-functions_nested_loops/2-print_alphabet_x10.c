53f8db197cc54aa371948e74c92ca953ec668b0e
mohammedchakir <zerroukimedchakir@gmail.com>
2023-07-18 05:57:37 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet in lowercase ten times
+ */
+void print_alphabet_x10(void)
+{
+    char letter;
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (letter = 'a'; letter <= 'z'; letter++)
+        {
+            _putchar(letter);
+        }
+    }
+    _putchar('\n');
+}
