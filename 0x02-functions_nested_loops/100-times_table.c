14ff03df760a9a0f2997ef5225d5381e254ebbcf
mohammedchakir <zerroukimedchakir@gmail.com>
2023-07-18 11:51:55 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,33 +6,34 @@
  */
 void print_times_table(int n)
 {
-	int i, j, product;
-
 	if (n < 0 || n > 15)
+		return;
+
+	int a, b, op;
+
+	for (a = 0; a <= n; a++)
 	{
-		for (i = 0; i <= n; i++)
+		for (b = 0; b <= n; b++)
 		{
-			for (j = 0; j <= n; j++)
+			op = a * b;
+			if (b == 0)
 			{
-				product = i * j;
-				if (j == 0)
-				{
-					_putchar('0' + product);
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-					if (product <= 9)
-						_putchar(' ');
-					if (product <= 99)
-						_putchar(' ');
-					_putchar('0' + (product / 100));
-					_putchar('0' + ((product / 10) % 10));
-					_putchar('0' + (product % 10));
-				}
+				_putchar('0' + op);
 			}
-			_putchar('\n');
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (op <= 9)
+					_putchar(' ');
+				if (op <= 99)
+					_putchar(' ');
+				_putchar('0' + (op / 100));
+				_putchar('0' + ((op / 10) % 10));
+				_putchar('0' + (op % 10));
+			}
 		}
+		_putchar('\n');
 	}
 }
+
