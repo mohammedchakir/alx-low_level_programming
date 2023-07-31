fc72718cf589c1ef74e92a885f2ca2f21ad212d4
mohammedchakir <zerroukimedchakir@gmail.com>
2023-07-31 15:28:57 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Searches a string for any of a set of bytes.
+ * @s: The string to search.
+ * @accept: The set of bytes to match.
+ *
+ * Return: Pointer to the byte in s that matches one of the bytes in accept,
+ *         or NULL if no such byte is found.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+		char *a = accept;
+
+		while (*a != '\0')
+		{
+			if (*s == *a)
+				return s;
+
+			a++;
+		}
+
+		s++;
+	}
+
+	return NULL;
+}
