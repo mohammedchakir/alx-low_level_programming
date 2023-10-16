e91cf40408f5f32ca067134f9c2e2ddba645ea7e
mohammedchakir <zerroukimedchakir@gmail.com>
2023-10-16 04:15:32 -0700

    correct last task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p /tmp  https://github.com/mohammedchakir/alx/raw/main/0x18%20C%20-%20Dynamic%20libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.so 
+wget -p /tmp  https://github.com/mohammedchakir/alx-low_level_programming/raw/master/0x18-dynamic_libraries/randomgiga.so
+export LD_PRELOAD=/tmp/randomgiga.so 
