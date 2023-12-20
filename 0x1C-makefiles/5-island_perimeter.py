61aa17754f2a23580f3d4ff8a977325791cbc74f
mohammedchakir <zerroukimedchakir@gmail.com>
2023-12-20 03:40:36 -0800

    correcting task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
+"""Grid function"""
 
 def island_perimeter(grid):
-    perimeter = 0
+    """
+    Calculate the perimeter of an island.
 
+    Args:
+    grid (list of list of int): A grid where 0 represents water and 1 represents land.
+
+    Returns:
+    int: The perimeter of the island.
+    """
+    perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 perimeter += 4
-
                 if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
                 if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 2
-
     return perimeter
