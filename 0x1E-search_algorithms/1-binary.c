a3e6a7d44410c0e38995f23a4c53abbb7132d1bf
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-02-05 17:18:37 +0100

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,5 +1,4 @@
 #include "search_algos.h"
-#include <stdio.h>
 
 /**
  * binary_search - Search for a value in a sorted array using binary search
@@ -11,17 +10,14 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	if (array == NULL)
-		return (-1);
-
 	size_t left = 0, right = size - 1;
-
 	while (left <= right)
 	{
 		size_t mid = (left + right) / 2;
-
+		size_t i;
+		
 		printf("Searching in array: ");
-		for (size_t i = left; i <= right; i++)
+		for (i = left; i <= right; i++)
 		{
 			printf("%d", array[i]);
 			if (i < right)
@@ -36,6 +32,5 @@ int binary_search(int *array, size_t size, int value)
 		else
 			right = mid - 1;
 	}
-	
 	return (-1);
 }
