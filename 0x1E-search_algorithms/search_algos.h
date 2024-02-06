b46fab8873f74fe6a87d1cae687586c9adf282b2
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-02-06 15:19:09 +0100

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -52,7 +52,7 @@ skiplist_t *linear_skip(skiplist_t *list, int value);
 listint_t *create_list(int *array, size_t size);
 void free_list(listint_t *list);
 void print_list(const listint_t *list);
-int advanced_binary_recursive(int *array, size_t low, size_t high, int value);
+int binary_search_recursion(int *array, size_t low, size_t high, int value);
 
 #endif /* SEARCH_ALGOS_H */
 
