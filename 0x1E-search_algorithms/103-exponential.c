45a24f4c4feb82bd479b2cf96f81864b83681fbd
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-02-06 04:01:24 +0100

    Update 103-exponential.c

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * binary_search - Perform binary search within a specified range
+ * _binary_search - Perform binary search within a specified range
  * @array: Pointer to the first element of the array
  * @low: Starting index of the range
  * @high: Ending index of the range
@@ -10,7 +10,7 @@
  * Return: Index where the value is located, or -1 if not found
  */
 
-int binary_search(int *array, size_t low, size_t high, int value)
+int _binary_search(int *array, size_t low, size_t high, int value)
 {
 
 	size_t i;
