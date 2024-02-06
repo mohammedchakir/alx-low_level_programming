30286b433982b92bcd01a6d12a211a630ff0536a
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-02-06 03:13:06 +0100

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,11 +11,12 @@
 int binary_search(int *array, size_t size, int value)
 {
 	size_t left = 0, right = size - 1;
-	size_t mid = (left + right) / 2;
 	size_t i;
 
 	while (left <= right)
 	{
+		size_t mid = (left + right) / 2;
+
 		printf("Searching in array: ");
 		for (i = left; i <= right; i++)
 		{
