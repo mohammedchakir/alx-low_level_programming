92838f63891a80b3720709b32f8f6791932132e7
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-02-06 15:32:30 +0100

    Update 104-advanced_binary.c

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -34,7 +34,7 @@ int advanced_binary_recursive(int *array, size_t low, size_t high, int value)
 }
 
 /**
- * advanced_binary - Search for value in sorted array using advanced binary search
+ * advanced_binary - Search for value in array using advanced binary search
  * @array: Pointer to the first element of the array
  * @size: Number of elements in the array
  * @value: Value to search for
