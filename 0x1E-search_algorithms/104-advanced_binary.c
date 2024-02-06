c18d05c54e5ecd9742c7685f2c6a343dc26000fa
mohammed chakir <129831433+mohammedchakir@users.noreply.github.com>
2024-02-06 15:22:18 +0100

    Update 104-advanced_binary.c

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -3,25 +3,6 @@
 int binary_search_recursion(int *array, size_t low, size_t high, int value);
 
 /**
- * advanced_binary - Search for value in sorted array using advced binry search
- * @array: Pointer to the first element of the array
- * @size: Number of elements in the array
- * @value: Value to search for
- *
- * Return: Index where the value is located, or -1 if not found
- */
-int advanced_binary(int *array, size_t size, int value)
-{
-	size_t low = 0;
-	size_t high = size - 1;
-
-	if (!array)
-		return (-1);
-
-	return (binary_search_recursion(array, value, low, high));
-}
-
-/**
  * binary_search_recursion - Recursive helper function for
  * advanced binary search
  * @array: Pointer to the first element of the array
@@ -55,4 +36,23 @@ int binary_search_recursion(int *array, size_t low, size_t high, int value)
 	}
 
 	return (-1);
+}
+
+/**
+ * advanced_binary - Search for value in sorted array using advced binry search
+ * @array: Pointer to the first element of the array
+ * @size: Number of elements in the array
+ * @value: Value to search for
+ *
+ * Return: Index where the value is located, or -1 if not found
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	size_t low = 0;
+	size_t high = size - 1;
+
+	if (!array)
+		return (-1);
+
+	return (binary_search_recursion(array, value, low, high));
 }
